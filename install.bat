b4f1737c0b092acc90c4bd867a164623e30607ac
Vespy <136964274+Vesperlolll@users.noreply.github.com>
2023-06-18 06:01:35 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,13 @@
+pip install requests
+pip install winshell
+pip install psutil
+pip install pypiwin32
+pip install pycryptodome
+pip install cryptography
+pip install pillow
+pip install discord_webhook
+pip install pyperclip
+pip install opencv-python
+pip install pyinstaller
+pip install undetected_chromedriver 
+pip install pynput
